dd9c5aac1596dbc378359dc4fa6ae7986c619666
JayJin <jvidorca@gmail.com>
2025-12-29 23:03:50 +0900

    Feat: Add role to user

--- common/auth.py
+++ common/auth.py
@@ -2,15 +2,29 @@ from datetime import datetime, timedelta
 from fastapi import HTTPException, status
 from jose import JWTError, jwt
 
+from enum import StrEnum
+
 SECRET_KEY = "THIS_IS_SUPER_SECRET_KEY"
 ALGORITHM = "HS256"
 
+# 토큰에 역할 정의
+class Role(StrEnum):
+    ADMIN = "admin"
+    USER = "user"
+    
+
 def create_access_token(
     payload: dict, 
+    role = Role,
     expires_delta: timedelta = timedelta(hours=6),
 ):
     expire = datetime.utcnow() + expires_delta
-    payload.update({"exp": expire})
+    payload.update(
+        {
+            "role": role,
+            "exp": expire,
+        }
+    )
     encoded_jwt = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
     
     return encoded_jwt
